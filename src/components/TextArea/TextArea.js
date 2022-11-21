14572477c7b6f8c3f1e2d2d4a0ac50be59572316
hariharan2017 <hariharan9876@gmail.com>
2022-11-21 23:46:09 +0530

    Added key to questions list

--- src/components/TextArea/TextArea.js
+++ src/components/TextArea/TextArea.js
@@ -2,7 +2,7 @@ import "./TextArea.scss";
 
 export default function TextArea ({ id, label, required, onChange, height = "100px", width = "100%"}) {
   return (
-    <div className="text-area-container" style={{ width: `${width}` }}>
+    <div className="text-area-container" key={id} style={{ width: `${width}` }}>
       <label className="text-area-label">{label}</label>
       <textarea className="text-area-styles" id={id} onChange={onChange} style={{ height: `${height}` }} />
     </div>
--- src/components/TextField/TextField.js
+++ src/components/TextField/TextField.js
@@ -3,7 +3,7 @@ import "./TextField.scss";
 
 export default function TextField({ id, type, label, required, width, onChange, disabled }) {
   return (
-    <div className="input-container" style={{ width }} key={id}>
+    <div className="input-container" key={id} style={{ width }}>
       {label && (
         <div className="input-label-container">
           <label className="input-label">{label}</label>
