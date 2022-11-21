0c20b35fbc2085e3ba473b8c179dac9483b6c3d1
hariharan2017 <hariharan9876@gmail.com>
2022-11-21 14:44:34 +0530

    Added Textarea component

---
+++ src/components/TextArea/TextArea.js
@@ -0,0 +1,10 @@
+import "./TextArea.scss";
+
+export default function TextArea ({ id, label, required, onChange, height = "100px", width = "100%"}) {
+  return (
+    <div className="text-area-container" style={{ width: `${width}` }}>
+      <label className="text-area-label">{label}</label>
+      <textarea className="text-area-styles" id={id} onChange={onChange} style={{ height: `${height}` }} />
+    </div>
+  )
+}
