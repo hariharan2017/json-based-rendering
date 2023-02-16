342ca0805ab3688f5d5db56d0b8d9e66110e5b0d
hariharan2017 <hariharan9876@gmail.com>
2023-02-16 14:33:30 +0530

    Created Material Radio

---
+++ src/components/MaterialRadio/MaterialRadio.js
@@ -0,0 +1,23 @@
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import { MATERIAL_MARGIN } from '../../constants/cssConstants';
+
+function MaterialRadio ({ id, title, name, options, value, onChange, width = "100%" }) {
+  return (
+    <FormControl sx={{ width, margin: MATERIAL_MARGIN }}>
+      <FormLabel id={id}>{title}</FormLabel>
+      <RadioGroup name={name} value={value} onChange={onChange}>
+        {options.map((option) => {
+          return (
+            <FormControlLabel value={option.id} label={option.label} control={<Radio />}/>
+          );
+        })}
+      </RadioGroup>
+    </FormControl>
+  );
+}
+
+export default MaterialRadio;
